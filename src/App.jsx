3dfcca5ca5f642065082c76589e9d5d1211fce20
Greg <g.rutherford10@gmail.com>
2019-07-05 21:11:55 +0100

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>Arnold Clark Tech-Test</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
