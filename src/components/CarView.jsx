7bf44abd74adcc1608687f32bbe553823cbc6248
Greg <g.rutherford10@gmail.com>
2019-07-08 12:44:37 +0100

    add functionality to limit list of cars to 6

--- src/components/CarView.jsx
+++ src/components/CarView.jsx
@@ -4,31 +4,33 @@ const CarView = props => {
   const myMonthlyPayment =
     (props.vehiclePrice - props.deposit) / (props.financeOption * 12);
 
-  console.log(props.searchResults.length);
-
   const carsAvailable = props.searchResults.filter(car =>
     car.salesInfo.pricing.hasOwnProperty("monthlyPayment")
   );
 
-  console.log(myMonthlyPayment);
+  const filteredList = carsAvailable
+    .slice(0, 6)
+    .filter(i => i.salesInfo.pricing.monthlyPayment < myMonthlyPayment)
+    .sort(
+      (a, b) =>
+        a.salesInfo.pricing.monthlyPayment - b.salesInfo.pricing.monthlyPayment
+    );
 
   return (
     <div id="car-view">
-      {carsAvailable
-        .filter(i => i.salesInfo.pricing.monthlyPayment < myMonthlyPayment)
-        .map((car, index) => {
-          return (
-            <div className="car-card" key={index}>
-              <h1>{car.title.name}</h1>
-              <h3>{car.title.variant}</h3>
-              <p>{car.branch.name}</p>
-              <ul>
-                <li>£{car.salesInfo.pricing.cashPrice} Cash Price</li>
-                <li>£{car.salesInfo.pricing.monthlyPayment} per month</li>
-              </ul>
-            </div>
-          );
-        })}
+      {filteredList.map((car, index) => {
+        return (
+          <div className="car-card" key={index}>
+            <h1>{car.title.name}</h1>
+            <h3>{car.title.variant}</h3>
+            <p>{car.branch.name}</p>
+            <ul>
+              <li>£{car.salesInfo.pricing.cashPrice} Cash Price</li>
+              <li>£{car.salesInfo.pricing.monthlyPayment} per month</li>
+            </ul>
+          </div>
+        );
+      })}
     </div>
   );
 };
